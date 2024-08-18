fd0287b16b8b506ebc5b684fefbfe17e3472ed38
emeli <emeli@DESKTOP-R18QUBI>
2024-08-17 21:57:41 -0300

    Implementação do padrão Strategy

--- AvaliacaoIII_20242.MODEL/src/br/ifba/inf011/aval2/App.java
+++ AvaliacaoIII_20242.MODEL/src/br/ifba/inf011/aval2/App.java
@@ -3,17 +3,20 @@ package br.ifba.inf011.aval2;
 import java.time.LocalDate;
 
 import br.ifba.inf011.aval2.model.Arquivo;
+import br.ifba.inf011.aval2.model.Conversor2Bin;
 import br.ifba.inf011.aval2.model.Credencial;
 import br.ifba.inf011.aval2.model.Entrada;
 import br.ifba.inf011.aval2.model.EntradaOperavel;
 import br.ifba.inf011.aval2.model.Pasta;
+import br.ifba.inf011.aval2.model.Stratagy.ConversorBinario;
+import br.ifba.inf011.aval2.model.Stratagy.ConversorContext;
+import br.ifba.inf011.aval2.model.Stratagy.ConversorHexa;
+import br.ifba.inf011.aval2.model.Stratagy.ConversorOctal;
 
 public class App {
 	
 	
 	public void runQ1() throws IllegalAccessException  {
-		
-		Credencial user01 = new Credencial("user01");
 		
 		EntradaOperavel a1 = new Arquivo("A1", LocalDate.now(), "00011000100011100000011111110101");
 		EntradaOperavel b1 = new Arquivo("B1", LocalDate.now(), "UM ARQUIVO TAMANHO GRANDE");
@@ -33,33 +36,52 @@ public class App {
 		b.addFilho(b1);
 		
 		c.addFilho(c1);
-
-		try {
-			b1.escrever(user01, "CINCO");
-		}catch(IllegalAccessException ex) {
-			System.out.println("NÃO FOI POSSIVEL ESCREVER EM A1");
-		}
-			
-		System.out.println(raiz.getNome() + ": " + raiz.getTamanho() + "K");
-
-		try {
-			b1.escrever(user01, "CINCO+2");
-		}catch(IllegalAccessException ex) {
-			System.out.println("NÃO FOI POSSIVEL ESCREVER EM A1");
-		}
 		
-		try {
-			System.out.println("B1: " + b1.ler(user01));
-		} catch (IllegalAccessException e) {
-			System.out.println("NÃO FOI POSSIVEL LER DE A1");
-		}			
 		
-		System.out.println(raiz.getNome() + ": " + raiz.getTamanho() + "K");
+		Credencial user01 = new Credencial("user01");
+		ConversorBinario conversorBinario = new ConversorBinario();
+		ConversorOctal conversorOctal = new ConversorOctal();
+		ConversorHexa conversorHexa = new ConversorHexa();
+		
+		System.out.println("\n---------- Binario -----------");
+
+		ConversorContext binario = new ConversorContext(conversorBinario); 
+		System.out.println(binario.converterString(a1.ler(user01)));
+		
+		System.out.println("\n---------- Octal -----------");
+		ConversorContext octal = new ConversorContext(conversorOctal); 
+		System.out.println(octal.codificar(b1.ler(user01)));
+
+		System.out.println("\n---------- Hexa -----------");
+		ConversorContext hexa = new ConversorContext(conversorHexa); 
+		System.out.println(hexa.codificar(c1.ler(user01)));
+		
+
+//		try {
+//			b1.escrever(user01, "CINCO");
+//		}catch(IllegalAccessException ex) {
+//			System.out.println("NÃO FOI POSSIVEL ESCREVER EM A1");
+//		}
+//			
+//		System.out.println(raiz.getNome() + ": " + raiz.getTamanho() + "K");
+//
+//		try {
+//			b1.escrever(user01, "CINCO+2");
+//		}catch(IllegalAccessException ex) {
+//			System.out.println("NÃO FOI POSSIVEL ESCREVER EM A1");
+//		}
+//		
+//		try {
+//			System.out.println("B1: " + b1.ler(user01));
+//		} catch (IllegalAccessException e) {
+//			System.out.println("NÃO FOI POSSIVEL LER DE A1");
+//		}			
+		
+		//System.out.println(raiz.getNome() + ": " + raiz.getTamanho() + "K");
 	}
 	
 	
 	public static void main(String[] args) throws IllegalAccessException {
-		System.out.println("Ian é viado!!!");
 		App app = new App();
 		app.runQ1();
 	}
